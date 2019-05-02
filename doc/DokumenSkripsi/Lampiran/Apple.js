28e6fb9573266bbd7196e92b027b524229b381a8
GeneralDevilX <evelynwijaya777@gmail.com>
2019-05-03 00:19:18 +0700

    perbaiki dokumen + tambah gambat dan lampiran

--- doc/DokumenSkripsi/Lampiran/Apple.js
+++
@@ -1,9 +0,0 @@
-function Apple(besarApel){
-	this.x;
-	this.y;
-	this.besarApel = besarApel;
-	
-	this.getBesarApel = function(){
-		return this.besarApel;
-	}
-}
---
+++ doc/DokumenSkripsi/Lampiran/KodeProgram/Apple.js
@@ -0,0 +1,15 @@
+function Apple(besarApel){
+	this.x;
+	this.y;
+	this.besarApel = besarApel;
+	
+	this.getBesarApel = function(){
+		return this.besarApel;
+	}
+	this.getX = function(){
+		return this.x;
+	}
+	this.getY = function(){
+		return this.y;
+	}
+}
